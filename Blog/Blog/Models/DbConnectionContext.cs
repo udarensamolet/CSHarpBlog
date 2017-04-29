16a3108bc0e02c1b31af037a9e70a5bb9622fc70
Rositsa Nenova <udarensamolet@yahoo.com>
2017-04-29 08:18:30 +0300

    v.3

---
+++ Blog/Blog/Models/DbConnectionContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class DbConnectionContext : DbContext
+    {
+        public DbConnectionContext() : base("name=dbContext")
+        {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges
+            <DbConnectionContext>());
+        }
+        public DbSet<ImageGallery> ImageGallery { get; set; }
+    }
+}
